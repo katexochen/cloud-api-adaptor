1798197fec528abd9058b5754f79b89474dfcd8f
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-09-28 18:14:29 +0200

    nix: init flake
    Adding a Nix flake to pin toolchain and dependencies for building OS images
    with mkosi.
    
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Cloud API Adaptor for Confidential Containers";
+
+  inputs = {
+    nixpkgsUnstable = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs =
+    { self
+    , nixpkgsUnstable
+    , flake-utils
+    }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+      let
+        pkgsUnstable = import nixpkgsUnstable { inherit system; };
+      in
+      {
+        devShells = {
+          # Shell for building podvm images with mkosi.
+          podvm-mkosi = pkgsUnstable.mkShell {
+            nativeBuildInputs = with pkgsUnstable; [
+              btrfs-progs
+              cryptsetup
+              dnf5
+              dosfstools
+              mkosi-full
+              mtools
+              rpm
+              squashfsTools
+              util-linux
+            ];
+          };
+        };
+
+        formatter = nixpkgsUnstable.legacyPackages.${system}.nixpkgs-fmt;
+      });
+}
